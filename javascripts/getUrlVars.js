93699adfd334feeecd8d500f35d403038100e80d
Sami Babat <sami.babat@gmail.com>
2016-07-15 14:51:19 +0300

    Create getUrlVars.js

---
+++ javascripts/getUrlVars.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+var name = getUrlVars()["name"]; 
+var surname = getUrlVars()["surname"]; 
+document.getElementById("p1").innerHTML = name;
+});
